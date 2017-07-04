01963e7d4ac6b243333d8d69c68bfec33568ba17
meamuri <meammf@yandex.ru>
2017-07-04 16:43:14 +0300

    now I can take all cities

--- src/main/scala/application/Job.scala
+++ src/main/scala/application/Job.scala
@@ -18,12 +18,8 @@ import utils.{DataUtils, SparkUtils}
   */
 object Job {
   def main(args: Array[String]) {
-//    val df = DataInit.loadDataWithBothSexes(SparkInit.getSparkSession)
-//    val new_df = df.select("value")
-//    println("\n\n")
-//    new_df.rdd.take(5).foreach(println)
     val all_df = DataInit.loadDataWithBothSexes(SparkInit.getSparkSession)
-    val df = DataUtils.groupByCountries(all_df)
-    df.take(5).foreach(println)
+    val df = DataUtils.getAllCitiesRows(all_df)
+    df.take(5) foreach println
   }
 }
--- src/main/scala/utils/DataUtils.scala
+++ src/main/scala/utils/DataUtils.scala
@@ -7,17 +7,28 @@ import org.apache.spark.sql.{DataFrame, RelationalGroupedDataset}
   * Модуль для работы с данными
   */
 object DataUtils {
-  def groupByCountries(all_data: DataFrame): RDD[(Any, List[Any])] = {
+  def chooseLastRecord(iterable: Iterable[List[Any]]): List[Any] = {
+    val l = iterable.toList
 
-    val df = all_data
-      .select("Country or Area", "City", "Year", "Value")
-        .rdd
-      .map(x => (
-        x(0),
-        List(x(1), x(2), x(3))
-        )
-      )
-    df
+    var res = l.head
+    var max_y =  0
+    for (e <- l) {
+      val curr_year = e(1) match {
+        case Some(x: Int) => x // this extracts the value in a as an Int
+        case _ => 0
+      }
+      if (curr_year > max_y) {
+        res = e
+        max_y = curr_year
+      }
+    }
+    res
   }
 
+  def getAllCitiesRows(all_data: DataFrame): RDD[(Any, List[Any])] = {
+    all_data.select("Country or Area", "City", "Year", "Value").rdd
+      .map(x => (x(1), List(x(0), x(2), x(3))))
+      .groupByKey()
+      .mapValues(x => chooseLastRecord(x))
+  }
 }
