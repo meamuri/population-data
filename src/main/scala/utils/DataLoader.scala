3dfec86001c2219a063c3797c1cf8fe0441710dd
meamuri <meammf@yandex.ru>
2017-07-14 14:01:04 +0300

    now app loads csv to rdd at once

--- src/main/scala/utils/DataLoader.scala
+++ src/main/scala/utils/DataLoader.scala
@@ -2,49 +2,26 @@ package utils
 
 import java.io.File
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import dao.{City, PartOfPeople}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
+import scala.util.Try
 
-/**
-  * Загрузчик данных из файлов csv
-  * Перед загрузкой данных, необходимо проверить наличие файлов
-  * функцией checkWorkFolder.
-  * В случае возвращения функцией неудачного результата,
-  * попытки загрузить данные приведут к исключительным ситуациям
-  */
 class DataLoader(val basePath: String = "data") {
   private val fileBoth = basePath + "/unsd-citypopulation-year-both.csv"
   private val fileDiff = basePath + "/unsd-citypopulation-year-fm.csv"
 
-  /**
-    * Загружает данные с информацией о населении обоих полов.
-    *
-    * Обертка над функцией loadData
-    * позволяющая абстрагироваться от передачи ей логического параметра,
-    * определяющего, какой файл загружать
-    * @param sparkSession сессия работы библиотеки Spark, с помощью которой
-    *                     будет произведено считывание данных CSV файла
-    * @return функция возвращает набор данных из соответствующего файла
-    *         в виде DataFrame
-    */
-  def loadDataWithBothSexes(sparkSession: SparkSession): DataFrame = {
-    loadData(isBoth = true, sparkSession)
+  def loadDataWithBothSexes(sc: SparkContext): RDD[City] = {
+    val data = loadData(isBoth = true, sc)
+    val cities = dataToCities(data)
+    cities
   }
 
-  /**
-    * Загружает данные с информацией о населении,
-    * где данные о разных полах представлены разными строками данных.
-    *
-    * Обертка над функцией loadData
-    * позволяющая абстрагироваться от передачи ей логического параметра,
-    * определяющего, какой файл загружать
-    * @param sparkSession сессия работы библиотеки Spark, с помощью которой
-    *                     будет произведено считывание данных CSV файла
-    * @return функция возвращает набор данных из соответствующего файла
-    *         в виде DataFrame
-    */
-  def loadDataWithDiffSexes(sparkSession: SparkSession): DataFrame = {
-    loadData(isBoth = false, sparkSession)
+  def loadDataWithDiffSexes(sc: SparkContext): RDD[City] = {
+    val data = loadData(isBoth = false, sc)
+    val cities = dataToCities(data)
+    cities
   }
 
   /**
@@ -59,23 +36,36 @@ class DataLoader(val basePath: String = "data") {
     file_with_both_data.exists() && file_with_diff_data.exists()
   }
 
-  /**
-    * Функция загрузки данных из csv файлов, хранящихся в каталоге проекта
-    * @param isBoth логическая переменная, пределяющая,
-    *               нужно ли загружать данные обоих полов, или нет
-    * @param sparkSession сессия работы библиотеки Spark, с помощью которой
-    *                     будет произведено считывание данных CSV файла
-    * @return функция возвращает набор данных из соответствующего файла
-    *         в виде DataFrame
-    */
-  private def loadData(isBoth: Boolean, sparkSession: SparkSession):
-  DataFrame = {
-    val path =  if (isBoth) { fileBoth } else { fileDiff }
-    val dataFrame = sparkSession.read
-      .format("csv")
-      .option("header", "true")
-      .option("mode", "DROPMALFORMED")
-      .csv(path)
-    dataFrame
+  private def selectUsefulRows(data: RDD[City], year: Int): RDD[City] = {
+    val tmp = data.map(city => (city.name, city.copy()))
+      .groupByKey()
+    val res = if (year == -1) {
+      tmp.mapValues(cities => cities.maxBy(_.year))
+    } else { // если нашли указанный год, берем за этот год, в противном случае самую свежую запись
+      tmp.mapValues(cities => cities.find(city => city.year == year).getOrElse(cities.maxBy(_.year)))
+    }
+    res.map(pair => pair._2.copy())
   }
-}
+
+  private def dataToCities(data: RDD[Map[String, String]]): RDD[City] = {
+    data.map(row =>  City(
+        country = Try(row.get("Country or Area").toString).getOrElse("null").replaceAll("\\.", ""),
+        name = Try(row.get("City").toString).getOrElse("null").replaceAll("\\.", ""),
+        year = Try(row.get("Year").toString.toInt).getOrElse(-1),
+        population = Try(row.get("Value").toString.toDouble).getOrElse(0.0),
+        sex = PartOfPeople.strToChar(Try(row.get("Sex").toString).getOrElse("b"))
+      ))
+  }
+
+  private def loadData(isBoth: Boolean, sparkContext: SparkContext):
+  RDD[Map[String, String]] = {
+    val path =  if (isBoth) { fileBoth } else { fileDiff }
+    val csv = sparkContext.textFile(path)
+    val data = csv.map(line => line.split(",").map(elem => elem.trim))
+    val header = data.first
+
+    val result = data.map(splits => { header.zip(splits).toMap })
+    result
+  }
+
+} // ... class DataLoader
