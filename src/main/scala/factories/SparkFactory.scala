93bc8d0ed44e6bb115769c54db8a5d985e4ea45f
meamuri <meammf@yandex.ru>
2017-07-15 16:08:59 +0300

    spark config was changed

--- src/main/scala/factories/SparkFactory.scala
+++ src/main/scala/factories/SparkFactory.scala
@@ -7,11 +7,15 @@ import org.apache.spark.sql.SparkSession
   * Создание Spark контекста, единого для всей программы
   */
 object SparkFactory {
-  private val conf = new SparkConf().setMaster("local").setAppName("Simple Application")
+  private val conf = new SparkConf()
+    .setMaster("local")
+    .setAppName("dsr")
+    .set("spark.cores.max", "4")
+
   private val sc = new SparkContext(conf)
   private val session = SparkSession.builder()
       .config(conf=conf)
-      .appName("spark session")
+      .appName("dsr")
       .getOrCreate()
 
   def getSparkContext: SparkContext = { sc }
