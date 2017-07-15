36619744f9bc1a8b87249b9feac95c205ff2c54f
meamuri <meammf@yandex.ru>
2017-07-15 14:03:05 +0300

    tests proof that selecting algorithm works correctly

--- src/test/scala/unit/SparkSuite.scala
+++ src/test/scala/unit/SparkSuite.scala
@@ -2,10 +2,13 @@ package unit
 
 import dao.City
 import factories.{Resources, SparkFactory}
+import helpers.Common
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import utils.{DataLoader, DataUtils, MongoUtils, SparkUtils}
+
+import scala.util.Try
 
 /**
   * Тесты модулей приложения.
@@ -16,26 +19,40 @@ import utils.{DataLoader, DataUtils, MongoUtils, SparkUtils}
   * Файлы содержат сильно укороченный набор исходных файлов
   */
 class SparkSuite extends FunSuite with BeforeAndAfter {
+  val fileBoth: String = Resources.getTestDataPath + Resources.getBothFilename
+  val fileDiff: String =  Resources.getTestDataPath + Resources.getDiffFilename
+  val loader = new DataLoader
+
+  var dataFrame: DataFrame = _
+  var cities: RDD[City] = _
+
   before {
+    dataFrame = loader.loadData(fileBoth, SparkFactory.getSparkSession)
+    cities = loader.selectBothRows(dataFrame, -1)
   }
 
   after {
 
   }
 
-  test("data file should contain info about 5 countries") {
+  test("data file should contain info about 16 cities") {
+    val res = cities.count()
+    assert(res === 16)
   }
 
-  test("algorithm should take me info about 5 countries") {
+  test("cities rdd with difference year param should be difference"){
+    cities = loader.selectBothRows(dataFrame, -1)
+    val recently = cities.filter(city => city.name == "Gyumri (Leninakan)")
+    val res = recently.collect()
+    assert(res.length === 1)
+    assert(res(0).year === 2011)
+
+    cities = loader.selectBothRows(dataFrame, 2009)
+    val target_year = cities.filter(city => city.name == "Gyumri (Leninakan)")
+    val target = target_year.collect()
+    assert(target.length === 1)
+    assert(target(0).year === 2009)
   }
 
-  test("only 3 countries should have cities-millionaires"){
-  }
-
-  test("data should contain only must recently information, if year = -1") {
-  }
-
-  test("data_loader should check, that folder contain data files") {
-  }
 
 }
