8ce2e49af278e7a456cb8108182d6b97d3bb5546
meamuri <meammf@yandex.ru>
2017-07-15 12:39:57 +0300

    top cities job was added

---
+++ src/main/scala/application/JobTop.scala
@@ -0,0 +1,29 @@
+package application
+
+import factories.{MongoFactory, Resources, SparkFactory}
+import utils.{DataLoader, MongoUtils, SparkUtils}
+
+import scala.util.Try
+
+object JobTop {
+  def main(args: Array[String]) {
+    val path = if (args.length == 0) { Resources.getDataPath } else { args(0) }
+    val year = if (args.length < 2) { Resources.getYear } else { Try(args(1).toInt).getOrElse(-1) }
+
+    val loader = new DataLoader(path)
+    if (!loader.checkWorkFolder){
+      println("По указанному пути нет необходимых для работы файлов!")
+      return
+    }
+
+    val all_df = loader.loadDataWithBothSexes(SparkFactory.getSparkSession)
+    val worker = new SparkUtils(all_df, year)
+    val keeper = new MongoUtils
+
+    val top5 = worker.getTop5cities
+    keeper.saveTop5(top5, MongoFactory.getTopCollection)
+
+    MongoFactory.closeConnection()
+    SparkFactory.closeSession()
+  }
+}
